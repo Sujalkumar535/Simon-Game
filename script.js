a8e9921daa5bf12b131c38db7b8fed2e00beb90d
Sujalkumar535 <sujalkumar9624@gmail.com>
2026-06-01 12:34:03 +0530

    Updated simon game

---
+++ script.js
@@ -0,0 +1,12 @@
+let gameseq=[];
+let usweseq=[];
+
+let start=false;
+let level=0;
+
+document.addEventListener("keypress",function(){
+    if(start==false){
+        console.log("Game is Started");
+        start=true;
+    }
+})
